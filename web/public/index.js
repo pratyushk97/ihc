5b80060998ed2cc33d31e1f60654ba6e61661f65
Matthew Chinn <mattchinn96@gmail.com>
2017-10-03 00:37:26 -0700

    point to react for client side

--- web/public/index.js
+++ web/public/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 /*
 app.get('/api/passwords', (req, res) => {
