97532caf6f2f520c262ac47ab31be9ee6cf2c20f
Matthew Chinn <mattchinn96@gmail.com>
2017-10-14 23:01:43 -0700

    use env variable for detecting dev

--- web/react/src/config.js
+++ web/react/src/config.js
@@ -1,4 +1,4 @@
 // Eventually make DEV dependent on env variables
-let DEV = false;
+let DEV = process.env.NODE_ENV === 'development';
 export let api_home = DEV ? 'http://localhost:5001/ihc-database/us-central1/api' : 
                             'https://us-central1-ihc-database.cloudfunctions.net/api';
