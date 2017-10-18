e0d149a36cfb822482c8df88fa350bfc8c9c38d5
Matthew Chinn <mattchinn96@gmail.com>
2017-10-18 15:11:37 -0700

    firebase helpers get users and get user updates works

--- web/react/src/Firebase.js
+++ web/react/src/Firebase.js
@@ -53,37 +53,52 @@ function getUserKey(hash, groupId) {
     .then(snapshot => snapshot.val())
 }
 
+/*
+ * Takes in an object with birthday, firstname, lastname fields
+ */
 function userHash(update) {
-  return update.birthday + update.firstname + "&&" + update.lastname;
+  return update.birthday + update.firstname + "&" + update.lastname;
+}
+
+/**
+ * Takes in string hash that matches form from above userHash function, converts back
+ * to a user
+ */
+function extractUserFromHash(hash) {
+  const bday = hash.slice(0, 8); // First 8 chars are bday format yyyymmdd
+  const firstname = hash.slice(8, hash.indexOf('&'));
+  const lastname = hash.slice(hash.indexOf('&') + 1);
+  return {
+    firstname: firstname,
+    lastname: lastname,
+    birthday: bday
+  };
 }
 
 // public functions ===================================
-
-/* Returns Promise of List of updates */
-export function getAllUpdates(groupId) {
-  const updatesRef = database.ref(`/groups/${groupId}/updates`);
-  updatesRef.once('value')
-    .then(snapshot => snapshot.val())
-    .then(updateKeys => {
-      let list = [];
-      updateKeys.forEach(updateKey => list.push(updateKey.val()));
-      return list;
-    });
-}
 
 /* Returns Promise of List of users for given groupId */
 export function getAllUsers(groupId) {
   const updatesRef = database.ref(`/groups/${groupId}/users`);
   return updatesRef.once('value')
-    .then(snapshot => snapshot.val() );
+    .then(snapshot => snapshot.val() )
+    .then(usersObj => Object.keys(usersObj).map(key => usersObj[key]))
+    .then(hashedUsers => hashedUsers.map(hashedUser =>
+          extractUserFromHash(hashedUser)))
 }
 
-/* Returns Promise of List of updates for that user */
-export function getUserUpdates(userKey, groupId) {
-  const updateKeysRef = database.ref(`/groups/${groupId}/user/${userKey}`);
+/*
+ * Returns Promise of List of updates for that user
+ * Takes in a user object with fields: firstname, lastname, birthday
+ */
+export function getUserUpdates(user, groupId) {
+  const hash = userHash(user);
+  const updateKeysRef = database.ref(`/groups/${groupId}/user/${hash}`);
   const updates = [];
   return updateKeysRef.once('value')
     .then(snapshot => snapshot.val() )
+    .then(val => val ? val : [])
+    .then(updateKeysObj => Object.keys(updateKeysObj))
     .then(updateKeys => {
       const promises = [];
       updateKeys.forEach(updateKey => {
--- web/react/src/UsersTable.js
+++ web/react/src/UsersTable.js
@@ -6,13 +6,14 @@ class UsersTable extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { users: [] };
+    this.state = { users: [], updates: [] };
     this.initUsers();
   }
 
   initUsers = () => {
     const groupId = '1';
-    firebase.getAllUsers(groupId).then(users => this.setState(users: users));
+    firebase.getAllUsers(groupId)
+      .then(users => this.setState({users: users}));
   }
 
   renderUsers = (users) => {
@@ -25,6 +26,19 @@ class UsersTable extends Component {
               </div>
               ))}
       </ol>
+    )
+  }
+
+  renderUpdates = (updates) => {
+    return (
+        <div>
+        {updates.map(update => (
+              <div className="update-row">
+                <span>{update.firstname}</span>
+                <span>{update.symptoms}</span>
+              </div>
+            ))}
+        </div>
     )
   }
 
@@ -44,14 +58,29 @@ class UsersTable extends Component {
     firebase.addUpdates([update, update2], 1);
   }
 
+  getUserUpdates = () => {
+    const user = {
+      firstname: "Matt",
+      lastname: "Chinn",
+      birthday: "19960807",
+    };
+
+    firebase.getUserUpdates(user, 1)
+      .then(updates => this.setState({updates: updates}));
+  }
+
   render() {
     const users = this.state.users;
+    const updates = this.state.updates;
 
     return(
         <div className="UsersTable">
           {users.length ? this.renderUsers(users) :
                           <span>No users found</span>}
           <button onClick={this.addUpdate}>Add Update</button>
+
+          <button onClick={this.getUserUpdates}>Get Updates</button>
+          {this.renderUpdates(updates)}
         </div>
         );
   }
