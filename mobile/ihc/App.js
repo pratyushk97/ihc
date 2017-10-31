c048630bba5a6f52e067d20250367812a89415cf
Matthew Chinn <mattchinn96@gmail.com>
2017-10-31 15:54:15 -0700

    ihc forms

--- mobile/ihc/App.js
+++ mobile/ihc/App.js
@@ -8,18 +8,44 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
+  ActivityIndicator,
+  Button,
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {API_HOME} from './config';
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    state = {text: "", loading: false};
+  }
+
+  uploadUpdates = () => {
+    this.setState({text: "Upload", loading: true});
+    /*
+      fetch(`${API_HOME}/groups/${groupId}/all`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstParam: 'yourValue',
+          secondParam: 'yourOtherValue',
+        })
+      })
+    */
+  }
+
+  downloadUpdates = () => {
+    this.setState({text: "Download", loading: false});
+  }
+
+  newPatient = () => {
+    this.setState({text: "New patient"});
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -27,11 +53,15 @@ export default class App extends Component<{}> {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          {this.state.text}
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <ActivityIndicator animating?={this.state.loading} />
+        <Button onPress={uploadUpdates}
+          title="Upload updates"/>
+        <Button onPress={downloadUpdates}
+          title="Download updates"/>
+        <Button onPress={newPatient}
+          title="New patient"/>
       </View>
     );
   }
