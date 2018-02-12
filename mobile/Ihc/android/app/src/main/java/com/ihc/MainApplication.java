dd5f0c6fe670c76b7f2539129c18ca94716eb62d
Matthew Chinn <mattchinn96@gmail.com>
2018-02-12 13:23:05 -0800

    link to react-native-svg

--- mobile/Ihc/android/app/src/main/java/com/ihc/MainApplication.java
+++ mobile/Ihc/android/app/src/main/java/com/ihc/MainApplication.java
@@ -33,6 +33,7 @@ public class MainApplication extends NavigationApplication {
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -52,7 +53,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SvgPackage()
       );
     }
 
