69e5750e43abbb3d4a9ab89632986e7f5e5d1d16
Matthew Chinn <mattchinn96@gmail.com>
2018-02-12 16:44:23 -0800

    update growtchchart data to array of points

--- mobile/Ihc/growthchartdata/extract_data.py
+++ mobile/Ihc/growthchartdata/extract_data.py
@@ -17,14 +17,22 @@ def convert(row):
 
 def extractPoints(obj):
     arr = []
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P3": obj["P3"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P5": obj["P5"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P10": obj["P10"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P25": obj["P25"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P50": obj["P50"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P75": obj["P75"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P90": obj["P90"]})
-    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P95": obj["P95"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P3": obj["P3"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P5": obj["P5"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P10": obj["P10"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P25": obj["P25"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P50": obj["P50"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P75": obj["P75"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P90": obj["P90"]})
+#    arr.append({"Sex": obj["Sex"], "Agemos": obj["Agemos"], "P95": obj["P95"]})
+    arr.append([obj["Agemos"], obj["P3"]])
+    arr.append([obj["Agemos"], obj["P5"]])
+    arr.append([obj["Agemos"], obj["P10"]])
+    arr.append([obj["Agemos"], obj["P25"]])
+    arr.append([obj["Agemos"], obj["P50"]])
+    arr.append([obj["Agemos"], obj["P75"]])
+    arr.append([obj["Agemos"], obj["P90"]])
+    arr.append([obj["Agemos"], obj["P95"]])
     return arr
 
 fieldnames = ("Sex", "Agemos", "L", "M", "S", "P3", "P5", "P10", "P25", "P50", \
