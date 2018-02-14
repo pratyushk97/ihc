dbb03bed13d3c71882c3e5931bb668bcc98447be
Matthew Chinn <mattchinn96@gmail.com>
2018-02-13 23:47:20 -0800

    update signin screen to scroll

--- mobile/Ihc/screens/SigninScreen.js
+++ mobile/Ihc/screens/SigninScreen.js
@@ -1,14 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
   Button,
   Text,
+  ScrollView,
   View
 } from 'react-native';
 var t = require('tcomb-form-native');
@@ -34,41 +29,38 @@ export default class SigninScreen extends Component<{}> {
     }
   }
 
-  goToWelcome = () => {
-    this.props.navigator.push({
-      screen: 'Ihc.WelcomeScreen',
-      title: 'Welcome'
-    });
-  }
-
   //TODO
   submit = () => {
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.title}>
           Signin
         </Text>
 
         <View>
           <Form ref="form" type={this.Signin}
+            style={styles.form}
             options={this.options}
           />
           <Button onPress={this.submit}
             title="Submit" />
-          <Button onPress={this.goToWelcome}
-            title="Back to home" />
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  form: {
+    width: '100%',
+    alignItems: 'flex-start'
+  },
   container: {
-    flex: 1,
+    flex: 0,
+    padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
