4e1013765d1557b5a578d51b2d51e893bd5a1d6f
Matthew Chinn <mattchinn96@gmail.com>
2018-04-10 11:17:05 -0700

    basic jest test works

--- mobile/Ihc/__tests__/WelcomeScreen.js
+++ mobile/Ihc/__tests__/WelcomeScreen.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+  const json = renderer.create(
+    <WelcomeScreen />
+  ).toJSON();
+  expect(json).toMatchSnapshot();
 });
