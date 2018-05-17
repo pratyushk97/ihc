eaa396d9234c581412f20f1d04228951785e061d
Matthew Chinn <mattchinn96@gmail.com>
2018-05-16 18:39:56 -0700

    fix tests after refactor

--- mobile/Ihc/__tests__/MedicationScreen.js
+++ mobile/Ihc/__tests__/MedicationScreen.js
@@ -2,10 +2,18 @@ import 'react-native';
 import React from 'react';
 import MedicationScreen from '../screens/MedicationScreen';
 
+import DrugUpdate from '../models/DrugUpdate';
+import sinon from 'sinon';
+import {localData} from '../services/DataService';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
+  const drug1 = DrugUpdate.getInstance();
+  const drug2 = DrugUpdate.getInstance();
+  sinon.stub(localData, 'getMedicationUpdates').returns([drug1, drug2]);
+
   const fakeNavigator = { setOnNavigatorEvent: (param) => {} };
   const json = renderer.create(
     <MedicationScreen navigator={fakeNavigator} />
--- mobile/Ihc/__tests__/PatientSelectScreen.js
+++ mobile/Ihc/__tests__/PatientSelectScreen.js
@@ -2,10 +2,25 @@ import 'react-native';
 import React from 'react';
 import PatientSelectScreen from '../screens/PatientSelectScreen';
 
+import Patient from '../models/Patient';
+import Status from '../models/Status';
+import {localData} from '../services/DataService';
+
+import sinon from 'sinon';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
+  const patient = Patient.getInstance();
+  const patient2 = Patient.getInstance();
+  const statuses = [Status.newStatus(patient), Status.newStatus(patient2)];
+  const columnOrder = ['name', 'birthday', 'checkinTime', 'triageCompleted',
+    'doctorCompleted', 'pharmacyCompleted', 'notes', 'patientKey'];
+
+  const rows = statuses.map((obj) => columnOrder.map( (key) => obj[key] ));
+
+  sinon.stub(localData, 'getPatientSelectRows').returns(rows);
+
   const fakeNavigator = { setOnNavigatorEvent: (param) => {} };
   const json = renderer.create(
     <PatientSelectScreen navigator={fakeNavigator} />
